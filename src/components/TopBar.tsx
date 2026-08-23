bb1fdf988c1dcaac18f06a34e20ed5271583dfcb
zzq120906-web <zzq120906@qq.com>
2026-08-23 09:56:44 +0800

    Create TopBar.tsx

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,26 @@
+export default function TopBar(){
+
+return(
+
+<div className="topbar">
+
+
+<div>
+🌐 SEA DOMINION
+</div>
+
+
+<div className="resources">
+
+💰 12500  
+⛽ 8000  
+🔬 250
+
+</div>
+
+
+</div>
+
+)
+
+}
