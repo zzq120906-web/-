d83d86e8bf5156d80280e56623356925381634f1
zzq120906-web <zzq120906@qq.com>
2026-08-23 10:01:56 +0800

    Create WorldMap.tsx

---
+++ src/map/WorldMap.tsx
@@ -0,0 +1,29 @@
+export default function WorldMap(){
+
+
+return(
+
+<div className="map">
+
+
+<div className="mapTitle">
+
+WORLD OCEAN COMMAND
+
+</div>
+
+
+<div className="placeholder">
+
+世界海洋战区
+
+</div>
+
+
+</div>
+
+
+)
+
+
+}
