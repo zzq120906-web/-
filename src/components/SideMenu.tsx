8174947f6122bdfb37bf1618a8ea5bf0ae13f7ab
zzq120906-web <zzq120906@qq.com>
2026-08-23 09:59:28 +0800

    Create SideMenu.tsx

---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,42 @@
+export default function SideMenu(){
+
+return(
+
+<div className="side">
+
+
+<button>
+⚓ 舰队
+</button>
+
+
+<button>
+🚀 导弹
+</button>
+
+
+<button>
+✈ 空军
+</button>
+
+
+<button>
+🛰 雷达
+</button>
+
+
+<button>
+🌐 全球情报
+</button>
+
+
+<button>
+⚙ 科技
+</button>
+
+
+</div>
+
+)
+
+}
